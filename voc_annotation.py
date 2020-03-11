e510cd7c150120693abf8a0f226e7a1e0fa58c5e
lujian <lujian1995@126.com>
2020-03-11 20:28:15 +0800

    transform voc annotation in YOLO
    1.修改voc_annotation.py，将自己生成的标注数据转换成YOLO格式要求的标注数据
    2.添加YOLO要求的标注数据20200311_train.txt等

--- voc_annotation.py
+++ voc_annotation.py
@@ -10,33 +10,53 @@ import xml.etree.ElementTree as ET
 from os import getcwd
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
-
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
+# 这是我自己的图片位置以及类别
+my_classes=['batteryCar', 'person', 'head']
+my_sets=[('20200311', 'train'), ('20200311', 'val'), ('20200311', 'test'),('20200311', 'trainval')]
+my_wd='D:/data/myYOLOdata'
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    '''
+    根据图片的ID写入该图片的gt数据
+    :param year: VOC数据年份
+    :param image_id: 图片id
+    :param list_file: 要写入的文件
+    :return: 无
+    '''
+    in_file = open('%s/VOC%s/Annotations/%s.xml'%(my_wd, year, image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in my_classes or int(difficult)==1:
             continue
-        cls_id = classes.index(cls)
+        cls_id = my_classes.index(cls)
         xmlbox = obj.find('bndbox')
         b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
-wd = getcwd()
+# wd = getcwd()
+#
+# for year, image_set in sets:
+#     image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+#     list_file = open('%s_%s.txt'%(year, image_set), 'w')
+#     for image_id in image_ids:
+#         list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
+#         convert_annotation(year, image_id, list_file)
+#         list_file.write('\n')
+#     list_file.close()
 
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+
+# 按照自己的文件生成label数据
+for year, image_set in my_sets:
+    image_ids = open('%s/VOC%s/ImageSets/Main/%s.txt'%(my_wd, year, image_set)).read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w')    #需要保存的YOLO形式的标注文件
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
+        list_file.write('%s/VOC%s/JPEGImages/%s.jpg'%(my_wd, year, image_id))  #先写入图片的真实路径
+        convert_annotation(year, image_id, list_file)   #再写入图片的GT信息
         list_file.write('\n')
     list_file.close()
-
